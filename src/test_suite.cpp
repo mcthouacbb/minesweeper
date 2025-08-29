226eddcfccc121eef87589b713a70fd41b804a8d
mcthouacbb <mcthouacbb@gmail.com>
2025-08-29 12:40:21 -0400

    Update formatting for test suite output(#5)
    Finished easy test suite
    Test suite took 0.0049949 seconds. 15816.1 cases/sec
    Passed: 79/79
    Failed: 0/79
    
    Finished medium test suite
    Test suite took 4.01931 seconds. 2.73679 cases/sec
    Passed: 11/11
    Failed: 0/11
    
    Finished hard test suite
    Test suite took 119.055 seconds. 0.0755955 cases/sec
    Passed: 9/9
    Failed: 0/9

--- src/test_suite.cpp
+++ src/test_suite.cpp
@@ -18,7 +18,7 @@ TestPosition TestPosition::fromImage(const BoardImage& image, const SolutionInfo
     return result;
 }
 
-void run_test_suite(TestSuite suite, Solver solver)
+void run_test_suite(TestSuite suite, Solver solver, bool verbose)
 {
     const std::vector<TestPosition>& positions = [&]()
     {
@@ -37,6 +37,9 @@ void run_test_suite(TestSuite suite, Solver solver)
 
     auto t1 = std::chrono::steady_clock::now();
 
+    uint32_t numPassed = 0;
+    uint32_t numFailed = 0;
+
     for (const auto& pos : positions)
     {
         BoardImageBuilder builder(pos.data);
@@ -46,10 +49,14 @@ void run_test_suite(TestSuite suite, Solver solver)
         BoardImage image = builder.build();
         SolutionInfo solution = solver(image).value();
 
-        std::cout << image.renderSolution(solution) << std::endl;
+        if (verbose)
+            std::cout << image.renderSolution(solution) << std::endl;
+
+        bool passed = true;
 
         if (pos.solutionInfo.numValidSolutions != solution.numValidSolutions)
         {
+            passed = false;
             std::cout << "Failed: number of valid mine configurations does not match" << std::endl;
             std::cout << "    Expected: " << pos.solutionInfo.numValidSolutions << std::endl;
             std::cout << "    Computed: " << solution.numValidSolutions << std::endl;
@@ -60,6 +67,7 @@ void run_test_suite(TestSuite suite, Solver solver)
         if (PointSet(pos.solutionInfo.mines.begin(), pos.solutionInfo.mines.end())
             != PointSet(solution.mines.begin(), solution.mines.end()))
         {
+            passed = false;
             std::cout << "Failed: did not correctly identify which squares were always mines"
                       << std::endl;
         }
@@ -67,13 +75,31 @@ void run_test_suite(TestSuite suite, Solver solver)
         if (PointSet(pos.solutionInfo.clears.begin(), pos.solutionInfo.clears.end())
             != PointSet(solution.clears.begin(), solution.clears.end()))
         {
+            passed = false;
             std::cout << "Failed: did not correctly identify which squares were always clear"
                       << std::endl;
         }
+
+        numPassed += passed;
+        numFailed += !passed;
     }
 
     auto t2 = std::chrono::steady_clock::now();
     double seconds = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
+    switch (suite)
+    {
+        case TestSuite::EASY:
+            std::cout << "Finished easy test suite" << std::endl;
+            break;
+        case TestSuite::MEDIUM:
+            std::cout << "Finished medium test suite" << std::endl;
+            break;
+        case TestSuite::HARD:
+            std::cout << "Finished hard test suite" << std::endl;
+            break;
+    }
     std::cout << "Test suite took " << seconds << " seconds. " << positions.size() / seconds
               << " cases/sec" << std::endl;
+    std::cout << "Passed: " << numPassed << "/" << (numPassed + numFailed) << std::endl;
+    std::cout << "Failed: " << numFailed << "/" << (numPassed + numFailed) << std::endl;
 }
