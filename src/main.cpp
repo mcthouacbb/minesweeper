e72644fdf0d55a2ac655766202140bd672ce673a
mcthouacbb <mcthouacbb@gmail.com>
2025-10-24 16:40:56 -0400

    Add simulation of solvers and games

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include "solvers/brute_force.h"
 #include "test_suite.h"
 #include "test_suite_gen.h"
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 void tryRenderSolution(const BoardImage& image)
 {
@@ -13,32 +15,79 @@ void tryRenderSolution(const BoardImage& image)
         std::cout << "No solution available" << std::endl;
 }
 
+enum class GameResult
+{
+    WIN,
+    LOSS,
+    TOO_COMPLEX,
+    UNSOLVABLE
+};
+
+GameResult simulateGame(uint32_t width, uint32_t height, uint32_t numMines)
+{
+    static std::random_device rd;
+    static std::mt19937 gen{rd()};
+    Board board{width, height};
+
+    MoveResult moveResult;
+    do
+    {
+        board.genMines(numMines);
+        std::uniform_int_distribution<uint32_t> distW(0, width - 1);
+        std::uniform_int_distribution<uint32_t> distH(0, height - 1);
+        moveResult = board.makeMove(Point{distW(gen), distH(gen)});
+    } while (moveResult == MoveResult::MINE);
+
+    while (board.numCleared() < board.width() * board.height() - board.numMines())
+    {
+        BoardImage image = board.genImage();
+        auto solution = solvers::basic_optimized::solve(image);
+        if (!solution.has_value())
+            return GameResult::TOO_COMPLEX;
+        if (solution->clears.empty())
+            return GameResult::UNSOLVABLE;
+
+        for (Point clear : solution->clears)
+        {
+            MoveResult moveResult = board.makeMove(clear);
+            if (moveResult == MoveResult::MINE)
+                return GameResult::LOSS;
+        }
+    }
+
+    return GameResult::WIN;
+}
+
+std::string toPercentStats(uint32_t success, uint32_t total)
+{
+    double frac = static_cast<double>(success) / static_cast<double>(total);
+    double stdev = std::sqrt(frac * (1.0 - frac) / total);
+    double errorBar = 2 * stdev;
+    std::ostringstream result;
+    result << std::fixed << std::setprecision(2);
+    result << frac * 100 << "% +/- " << errorBar * 100 << "%";
+    return result.str();
+}
+
+void simulateGames(uint32_t width, uint32_t height, uint32_t numMines, uint32_t games = 100)
+{
+    std::array<uint32_t, 4> results = {};
+    for (uint32_t simul = 0; simul < games; simul++)
+    {
+        GameResult result = simulateGame(width, height, numMines);
+        results[static_cast<uint32_t>(result)]++;
+    }
+    std::cout << "WIDTH: " << width << " HEIGHT: " << height << " MINES: " << numMines
+              << " SOLVED: " << toPercentStats(results[0], games)
+              << " TOO_COMPLEX: " << toPercentStats(results[2], games)
+              << " UNSOLVABLE: " << toPercentStats(results[3], games) << std::endl;
+}
+
 int main()
 {
-    // Board board(20, 10);
-    // board.genMines(40);
-    // std::cout << board << std::endl;
-    // std::cout << board.genImage() << std::endl;
-
-    // tryRenderSolution(board.genImage());
-
-    // board.makeMove({7, 6});
-    // std::cout << board << std::endl;
-    // std::cout << board.genImage() << std::endl;
-
-    // tryRenderSolution(board.genImage());
-
-    // board.makeMove({4, 4});
-    // std::cout << board << std::endl;
-    // std::cout << board.genImage() << std::endl;
-
-    // tryRenderSolution(board.genImage());
-
-    // // board.makeMove({11, 9});
-    // // std::cout << board << std::endl;
-    // // std::cout << board.genImage() << std::endl;
-
-    // // tryRenderSolution(board.genImage());
+    simulateGames(9, 9, 10, 5000);
+    simulateGames(16, 16, 40, 5000);
+    simulateGames(30, 16, 99, 5000);
 
     // BoardImageBuilder builder({15, 15, 35});
     // builder.addClearedCell({3, 3}, 0);
@@ -81,9 +130,9 @@ int main()
 
     // generateTestSuite();
 
-    run_test_suite(TestSuite::EASY, solvers::basic_optimized::solve);
-    run_test_suite(TestSuite::MEDIUM, solvers::basic_optimized::solve);
-    run_test_suite(TestSuite::HARD, solvers::basic_optimized::solve);
+    // run_test_suite(TestSuite::EASY, solvers::basic_optimized::solve);
+    // run_test_suite(TestSuite::MEDIUM, solvers::basic_optimized::solve);
+    // run_test_suite(TestSuite::HARD, solvers::basic_optimized::solve);
 
     // run_test_suite(TestSuite::EASY, solvers::brute_force::solve);
     // run_test_suite(TestSuite::MEDIUM, solvers::brute_force::solve);
